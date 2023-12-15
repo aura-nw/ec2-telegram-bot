ca822b72f97fa559b6b6f44103c3bd2145bd36a8
kienvc <vuchikien269@gmail.com>
2023-12-15 17:06:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ def main() -> None:
 
     # Create the Updater and pass it your bot's token
     global updater
-    updater = Updater(token)
+    updater = Updater(token, use_context=True))
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
