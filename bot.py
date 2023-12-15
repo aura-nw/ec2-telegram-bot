0b98e45e717f0fe55b55162c8c009a7d897321aa
kienvc <vuchikien269@gmail.com>
2023-12-15 18:14:01 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ def list_instances(update: Update, context: CallbackContext) -> None:
             instances_info.append(f"ID: `{instance_id}`, {instance_name}, {instance_state}")
     
     reply_text = '\n'.join(instances_info)
-    update.message.reply_markdown(reply_text, parse_mode=ParseMode.MARKDOWN)
+    update.message.reply_markdown_v2(reply_text)
 
 @log_request
 @check_user
